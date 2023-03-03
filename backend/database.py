2a01e93f840da7bc01f7600191511825ba4d00fa
harry <hlr286@nyu.edu>
2023-03-03 03:34:27 -0500

    comment

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:19065RA2y@localhost/foodshare_db"
+# MOVE META DATA TO YAML FILE
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/foodshare"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
