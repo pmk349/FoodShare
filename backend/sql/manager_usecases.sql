6280f2a528fef08bffce0d1439019810a55b7b52
harry <hlr286@nyu.edu>
2023-03-03 19:46:16 -0500

    fixed sql logic

--- backend/sql/manager_usecases.sql
+++ backend/sql/manager_usecases.sql
@@ -69,12 +69,13 @@ where id={transaction_id}
     # 5. view transaction history
 
 -- get transaction history
-select case
-    when t.anonymous = True then
-    ('anonymous shopper', t.request_time, t.request_action, t.request_status, t.request_summary)
-    when t.anonymous != True then
-    (a.name, t.request_time, t.request_action, t.request_status, t.request_summary)
+select
+	(case
+    when t.anonymous then 'anonymous shopper'
+    when not t.anonymous then a.name
+	end) as shopper, t.request_time, t.request_action, t.request_status, t.summary, t.quantity
 from transaction_request as t, pantry as p, account as a
 where t.pantry_id = p.id
   and t.shopper_id = a.id
-  and p.manager_id = {ID}
+  and p.manager_id = {MANAGER_ID}
+
